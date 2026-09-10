bf47dc1dd17363b482b2fc18e7122e26bdc794dc
Manav Jagtap <jmanavd@gmail.com>
2026-09-11 00:07:40 +0530

    feat: add QR ticket page for confirmed bookings

--- registrations/views.py
+++ registrations/views.py
@@ -1,3 +1,7 @@
+import base64
+from io import BytesIO
+
+import qrcode
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied, ValidationError
@@ -30,10 +34,15 @@ def register_event(request, event_id):
 
     try:
         registration = book_event(request.user, event.pk)
+
         if registration.status == Registration.Status.CONFIRMED:
             messages.success(request, "Your seat is confirmed!")
         else:
-            messages.info(request, "Event is full. You have joined the waitlist.")
+            messages.info(
+                request,
+                "Event is full. You have joined the waitlist."
+            )
+
     except ValidationError as exc:
         messages.error(request, "; ".join(exc.messages))
 
@@ -54,6 +63,47 @@ def my_bookings(request):
 
 
 @attendee_required
+def ticket_detail(request, ticket_code):
+    registration = get_object_or_404(
+        Registration.objects.select_related(
+            "event",
+            "event__category",
+            "event__organizer",
+        ),
+        ticket_code=ticket_code,
+        user=request.user,
+        status=Registration.Status.CONFIRMED,
+    )
+
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=8,
+        border=3,
+    )
+
+    qr.add_data(str(registration.ticket_code))
+    qr.make(fit=True)
+
+    image = qr.make_image(fill_color="black", back_color="white")
+
+    buffer = BytesIO()
+    image.save(buffer, format="PNG")
+
+    qr_image = base64.b64encode(
+        buffer.getvalue()
+    ).decode("utf-8")
+
+    return render(
+        request,
+        "registrations/ticket_detail.html",
+        {
+            "registration": registration,
+            "qr_image": qr_image,
+        },
+    )
+
+
+@attendee_required
 @require_POST
 def cancel_booking(request, registration_id):
     registration = get_object_or_404(
@@ -64,7 +114,11 @@ def cancel_booking(request, registration_id):
 
     try:
         cancel_registration(request.user, registration.pk)
-        messages.success(request, "Your registration has been cancelled.")
+        messages.success(
+            request,
+            "Your registration has been cancelled."
+        )
+
     except ValidationError as exc:
         messages.error(request, "; ".join(exc.messages))
 
